d84f183c35f9b2eb1483ac6d5cc94f3f1465409a
s-nakajima <nakajimashouhei@gmail.com>
2015-08-06 15:28:33 +0900

    Fix by develop specification of rooms

---
+++ Model/PrivateSpace.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * PrivateSpace Model
+ *
+ * @property Space $Space
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('Space', 'Rooms.Model');
+
+/**
+ * PublicSpace Model
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\PrivateSpace\Model
+ */
+class PrivateSpace extends Space {
+
+}
